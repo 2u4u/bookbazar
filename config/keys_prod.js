a504c2fcb12384ad87b845adbaffc58c24948eb5
U U <u.ulugbek@gmail.com>
2020-05-11 19:14:12 +0300

    added keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,3 +1,16 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
+  google: {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
+  },
+  facebook: {
+    clientID: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+  },
+  session: {
+    secret: process.env.SESSION_SECRET,
+    name: process.env.SESSION_NAME,
+  }
 };
